5f22101038b20a22d5a16253d0b54b07383219c5
vattgern <sashawot060@gmail.com>
2022-05-13 13:12:19 +0400

    Api Back

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 require 'connect.php';
 require 'functions.php';
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Credentials: true');
 header("Content-Type: json/application");
 
 $method = $_SERVER['REQUEST_METHOD'];
